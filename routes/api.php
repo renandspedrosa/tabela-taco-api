eea7eaabfb16fcd6fca412fa1485330fdca2f181
Renan Pedrosa <renandspedrosa@gmail.com>
2023-07-21 08:48:27 -0300

    sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IbgesController;
 /*
@@ -18,6 +20,22 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('/alimentos/ibge',IbgesController::class);
+Route::middleware('auth:sanctum')->group(function(){
+    Route::apiResource('/alimentos/ibge',IbgesController::class);
+    
+    Route::patch('/alimentos/ibge/{$id}',[IbgesController::class,'status']);
+});
 
-Route::patch('/alimentos/ibge/{$id}',[IbgesController::class,'status']);
+
+Route::post('/login',function(Request $request){
+    $credentials = $request->only(['email','password']);
+    $user = User::whereEmail($credentials['email'])->first();
+
+
+    if(Auth::attempt($credentials) === false) return response()->json('Unauthorized',401);
+    
+    $user = Auth::user();
+    $user->tokens()->delete();
+    $token = $user->createToken('token');
+    return response()->json($token->plainTextToken);
+});
